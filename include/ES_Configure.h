d7ad27a0ca80627d9e77e5ea81c367a98c640109
Justin Ewing <jpewing@ucsc.edu>
2015-11-18 18:04:51 -0800

    Config [UPDATED]
    timer shit

--- include/ES_Configure.h
+++ include/ES_Configure.h
@@ -78,12 +78,12 @@ static const char *EventNames[] = {
 
 /****************************************************************************/
 // This is the list of event checking functions
-#define EVENT_CHECK_LIST  CheckTrackWire,   \
-                          CheckBeacon,      \
-                          CheckTopTape,     \
-                          CheckLeftTape,    \
-                          CheckRightTape,   \
-                         CheckLauncherTape, \
+#define EVENT_CHECK_LIST  CheckTrackWire,    \
+                          CheckBeacon,       \
+                          CheckTopTape,      \
+                          CheckLeftTape,     \
+                          CheckRightTape,    \
+                          CheckLauncherTape, \
 
 
 /****************************************************************************/
@@ -94,11 +94,11 @@ static const char *EventNames[] = {
 #define TIMER0_RESP_FUNC PostTimerService
 #define TIMER1_RESP_FUNC PostTimerService
 //user modifiable timers start below here
-#define TIMER2_RESP_FUNC TIMER_UNUSED  //These are for?
-#define TIMER3_RESP_FUNC TIMER_UNUSED  //These are for?
+#define TIMER2_RESP_FUNC TIMER_UNUSED  
+#define TIMER3_RESP_FUNC Post_R2_BJT2_HSM    //backup timer
 #define TIMER4_RESP_FUNC PostR2BumperService
-#define TIMER5_RESP_FUNC Post_R2_BJT2_HSM
-#define TIMER6_RESP_FUNC Post_R2_BJT2_HSM
+#define TIMER5_RESP_FUNC TIMER_UNUSED
+#define TIMER6_RESP_FUNC TIMER_UNUSED
 #define TIMER7_RESP_FUNC TIMER_UNUSED
 #define TIMER8_RESP_FUNC TIMER_UNUSED
 #define TIMER9_RESP_FUNC TIMER_UNUSED
